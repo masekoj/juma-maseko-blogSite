30680e567fc3eef41234553e95f717539ae7e8ce
masekoj <132418845+masekoj@users.noreply.github.com>
2026-02-01 05:32:11 +0200

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   base: '/juma-maseko-blogSite
-/'
+/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
