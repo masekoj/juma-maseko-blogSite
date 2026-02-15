55eb596b39eb40c672268bc08d09e3623fede396
masekoj <132418845+masekoj@users.noreply.github.com>
2026-02-15 12:06:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/juma-maseko-blogSite/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
