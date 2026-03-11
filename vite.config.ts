bafc86e7765bbce8be5950598af5207d3cc69984
masekoj <132418845+masekoj@users.noreply.github.com>
2026-03-11 12:12:18 +0000

    Change base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: '/',
+  base: '/juma-maseko-blogSite/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
